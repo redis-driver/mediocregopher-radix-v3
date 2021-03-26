79fa83b1369138003dc6d1ee8c1e3ad25adb92e0
nimoc <nimocc@126.com>
2021-03-26 11:23:15 +0800

    driver

---
+++ driver.go
@@ -0,0 +1,54 @@
+package radix3
+
+import (
+	"context"
+	"github.com/mediocregopher/radix/v3"
+	"errors"
+	redScript "github.com/redis-driver/script"
+	"log"
+	"strings"
+)
+
+type Client struct {
+	Core radix.Client
+	debug bool
+}
+// 为了防止调试之后忘记去掉 DebugOnce, 函数签名返回 error 可以让代码在编译期检查提示有错误未处理，实际上DebugOnce 永远返回 nil。
+func (c *Client) DebugOnce() error {
+	c.debug = true
+	return nil
+}
+func (c *Client) logDebug(cmd []string) {
+	if c.debug {
+		c.debug = false
+		log.Print("goclub/redis:(debug) exec: ", strings.Join(cmd, " "))
+	}
+}
+func (c Client) RedisCommand(ctx context.Context, valuePtr interface{}, args []string) (result struct { IsNil bool }, err error){
+	c.logDebug(args)
+	data := radix.MaybeNil{Rcv: valuePtr}
+	var moreArg []string
+	if len(args) >1 { moreArg = args[1:] }
+	err = c.Core.Do(radix.Cmd(&data, args[0], moreArg...)) ; if err != nil {
+		return
+	}
+	result.IsNil = data.Nil
+	return
+}
+
+func (c Client)  RedisScript (ctx context.Context, script redScript.Script) (result struct { IsNil bool }, err error){
+	data := radix.MaybeNil{Rcv: script.ValuePtr}
+	err = c.Core. Do(radix.NewEvalScript(len(script.Keys), script.Script).Cmd(&data, append(script.Keys, script.Args...)...)) ; if err != nil {
+		return
+	}
+	result.IsNil = data.Nil
+	return
+}
+
+
+func (c Client)  Close () error {
+	if c.Core == nil {
+		return errors.New("radix client is nil can not close")
+	}
+	return c.Core.Close()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/redis-driver/mediocregopher-radix-v3
+
+go 1.15
+
+require (
+	github.com/mediocregopher/radix/v3 v3.7.0
+	github.com/redis-driver/script v0.0.0-20210218124209-40ab117c8db3
+)
